cc93ba35838b387bd3cd05efc90391e7fb3f0451
Gennady Naumenko <ts_eliot@mail.ru>
2017-05-11 08:41:05 +0300

    VD implementation

--- client/index.js
+++ client/index.js
@@ -1,49 +1,49 @@
-let store = [
-    {
-        id: 1,
-        name: 'Node 1',
-        isChecked: false,
-        childs: [{
-            id: 11,
-            name: 'Node 1.1',
-            isChecked: true,
-        }]
-    },
-    {
-        id: 2,
-        name: 'Node 2',
-        isChecked: false,
-        childs: [{
-            id: 21,
-            name: 'Node 2.1',
-            isChecked: true,
-            childs: [{
-                id: 211,
-                name: 'Node 2.1.1',
-                isChecked: true,
-            }]
-        },{
-            id: 22,
-            name: 'Node 2.2',
-            isChecked: true,
-            childs: [{
-                id: 221,
-                name: 'Node 2.2.1',
-                isChecked: true,
-            }]
-        }]
-    }
-]
+// let store = [
+//     {
+//         id: 1,
+//         name: 'Node 1',
+//         isChecked: false,
+//         childs: [{
+//             id: 11,
+//             name: 'Node 1.1',
+//             isChecked: true,
+//         }]
+//     },
+//     {
+//         id: 2,
+//         name: 'Node 2',
+//         isChecked: false,
+//         childs: [{
+//             id: 21,
+//             name: 'Node 2.1',
+//             isChecked: true,
+//             childs: [{
+//                 id: 211,
+//                 name: 'Node 2.1.1',
+//                 isChecked: true,
+//             }]
+//         },{
+//             id: 22,
+//             name: 'Node 2.2',
+//             isChecked: true,
+//             childs: [{
+//                 id: 221,
+//                 name: 'Node 2.2.1',
+//                 isChecked: true,
+//             }]
+//         }]
+//     }
+// ]
 
-let oneNode = {
-    id: 11123,
-    name: 'Node 1.1.2.2',
-    isChecked: true,
-};
+// let oneNode = {
+//     id: 11123,
+//     name: 'Node 1.1.2.2',
+//     isChecked: true,
+// };
 
 /** @jsx createElem */
 
-function createElem(type, props, ...children) {
+function createElem( type, props, ...children) {
     return { type, props, children };
 }
 
@@ -55,116 +55,128 @@ const navPanel = (
     </div>
 );
 
-
-// const store = (
-//     <form class="form-panel">
-//         <div class="form-group">
-//             <label htmlFor="">Node 1</label>
-//             <input type="checkbox" id="1" name='Node 1' checked={false} />
-//            
-//             <div class="form-group">
-//                 <label htmlFor="">Node 1.1</label>
-//                 <input type="checkbox" id="1.1" name='Node 1.1' checked={true} />
-//             </div>
-//         </div>
-//
-//         <div class="form-group">
-//             <label htmlFor="">Node 2</label>
-//             <input type="checkbox" id="2" name='Node 2' checked={false} />
-//
-//             <div class="form-group">
-//                 <label htmlFor="">Node 2.1</label>
-//                 <input type="checkbox" id="2.1" name='Node 2.1' checked={true} />
-//
-//                 <div class="form-group">
-//                     <input type="checkbox" id="2.1.1" name='Node 2.1.1' checked={false} />
-//                 </div>
-//
-//                 <div class="form-group">
-//                     <input type="checkbox" id="2.1.2" name='Node 2.1.2' checked={true} />
-//                 </div>
-//             </div>
-//
-//             <div class="form-group">
-//                 <input type="checkbox" id="2.2" name='Node 2.2' checked={true} />
-//
-//                 <div class="form-group">
-//                     <input type="checkbox" id="2.2.1" name='Node 2.2.1' checked={false} />
-//                 </div>
-//
-//                 <div class="form-group">
-//                     <input type="checkbox" id="2.2.2" name='Node 2.2.2' checked={true} />
-//                 </div>
-//             </div>
-//         </div>
-//     </form>
-// );
+const oneNode = (
+    <div id="test" class="form-group">
+        <label htmlFor="">Node 123123</label>
+        <input type="checkbox" id="212312" name='Node 1.2.3.3.' checked={false} />
+    </div>
+)
 
 
-function createElement(node) {
-    if (typeof node === 'string') {
-        return document.createTextNode(node);
-    }
-    const $el = document.createElement(node.type);
-    if(node.props.id) {
-        $el.id = node.props.id;
+const store = (
+    <form id="form-root" class="form-panel">
+        <div class="form-group">
+            <label htmlFor="">Node 1</label>
+            <input type="checkbox" id="1" name='Node 1' checked={false} />
+           
+            <div class="form-group">
+                <label htmlFor="">Node 1.1</label>
+                <input type="checkbox" id="11" name='Node 1.1' checked={true} />
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label htmlFor="">Node 2</label>
+            <input type="checkbox" id="2" name='Node 2' checked={false} />
+
+            <div class="form-group">
+                <label htmlFor="">Node 2.1</label>
+                <input type="checkbox" id="21" name='Node 2.1' checked={true} />
+
+                <div class="form-group">
+                    <label htmlFor="">Node 2.1.1</label>
+                    <input type="checkbox" id="211" name='Node 2.1.1' checked={false} />
+                </div>
+
+                <div class="form-group">
+                    <label htmlFor="">Node 2.1.2</label>
+                    <input type="checkbox" id="212" name='Node 2.1.2' checked={true} />
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label htmlFor="">Node 2.2</label>
+                <input type="checkbox" id="22" name='Node 2.2' checked={true} />
+
+                <div class="form-group">
+                    <label htmlFor="">Node 2.2.1</label>
+                    <input type="checkbox" id="221" name='Node 2.2.1' checked={false} />
+                </div>
+
+                <div class="form-group">
+                    <label htmlFor="">Node 2.2.2</label>
+                    <input type="checkbox" id="222" name='Node 2.2.2' checked={true} />
+                </div>
+            </div>
+        </div>
+    </form>
+);
+
+class Render {
+    _changeNode (node1, node2) {
+        return typeof node1 !== typeof node2 ||
+            typeof node1 === 'string' && node1 !== node2 || node1.type !== node2.type
     }
     
-    // if(node.type === 'input') {
-    //     $el.type = node.props.type;
-    //     $el.name = node.props.name;
-    //     $el.checked = node.props.checked;
-    //
-    //     console.log($el)
-    //    
-    //     $el.addEventListener('change', (e) => {
-    //         console.log(e.target.id)
-    //     });
-    // }
-    
-    node.children.map(createElement).forEach($el.appendChild.bind($el));
-    return $el;
+    _createNode = (node) => {
+        if (typeof node === "string") {
+            return document.createTextNode(node);
+        }
+
+        const $el = document.createElement(node.type);
+        
+        if (node.props.id) {
+            $el.id = node.props.id;
+        }
+
+        if (node.type === 'input') {
+            $el.type = node.props.type;
+            $el.name = node.props.name;
+            $el.checked = node.props.checked;
+
+            // console.log($el)
+            //
+            // $el.addEventListener('change', (e) => {
+            //     console.log(e.target.id)
+            // });
+        }
+
+        node.children.map(this._createNode).forEach($el.appendChild.bind($el));
+        return $el;
+    }
+
+    _updateNode = ($parent, newNode, oldNode, index) => {
+        if (!oldNode) {
+            $parent.appendChild(
+                this._createNode(newNode)
+            );
+        } else if (!newNode) {
+            $parent.removeChild(
+                $parent.childNodes[index]
+            );
+        } else if (this._changeNode(newNode, oldNode)) {
+            $parent.replaceChild(
+                this._createNode(newNode),
+                $parent.childNodes[index]
+            );
+        } else if (newNode.type) {
+            const newLength = newNode.children.length;
+            const oldLength = oldNode.children.length;
+            for (let i = 0; i < newLength || i < oldLength; i++) {
+                this._updateNode(
+                    $parent.childNodes[index],
+                    newNode.children[i],
+                    oldNode.children[i],
+                    i
+                );
+            }
+        }
+    }
 }
 
-// function changed(node1, node2) {
-//     return typeof node1 !== typeof node2 ||
-//         typeof node1 === 'string' && node1 !== node2 ||
-//         node1.type !== node2.type
-// }
-
-// function updateElement($parent, newNode, oldNode, index = 0) {
-//
-//     console.log(oldNode)
-//    
-//     if (!oldNode) {
-//         $parent.appendChild(
-//             createElement(newNode)
-//         );
-//     } else if (!newNode) {
-//         $parent.removeChild(
-//             $parent.childNodes[index]
-//         );
-//     } else if (changed(newNode, oldNode)) {
-//         $parent.replaceChild(
-//             createElement(newNode),
-//             $parent.childNodes[index]
-//         );
-//     } else if (newNode.type) {
-//         const newLength = newNode.children.length;
-//         const oldLength = oldNode.children.length;
-//         for (let i = 0; i < newLength || i < oldLength; i++) {
-//             updateElement(
-//                 $parent.childNodes[index],
-//                 newNode.children[i],
-//                 oldNode.children[i],
-//                 i
-//             );
-//         }
-//     }
-// }
-
-class App {
+class App extends Render {
     constructor(root, state) {
+        super()
         this._state = state;
         this._root = root;
     }
@@ -182,67 +194,59 @@ class App {
     updateNode (id, node) {
         if (id && isNaN(id)) throw new Error('Id must be a number!');
         if(!(typeof node === 'object')) throw new Error('Node must be an object');
-        
-        let trigger = false;
+        let trigger = false, oldState = this._state;
 
         function parseState(state) {
+            
             for(let i = 0; i < state.length; i++) {
-                if(state[i].id === id) {
-                    trigger = true;
-                    state[i] = node;
-                    break
-                } else {
-                    if(state[i].hasOwnProperty('childs') && state[i].childs.length > 0) {
-                        parseState(state[i].childs)
-                        if(trigger) {
-                            break 
+                if(state[i].type) {
+
+                    if(state[i].props.id && state[i].props.id === id+'') {
+                        trigger = true;
+                        state[i] = node;
+                        break
+                    }
+
+                    if(state[i].hasOwnProperty('children') && state[i].children.length > 0) {
+                        parseState(state[i].children)
+                        if (trigger) {
+                            break
                         }
                     }
                 }
             }
         }
         
-        parseState(this._state)
+        parseState(this._state.children)
+        this._updateNode(this._root, this._state, oldState, 1);
+        this.render();
     }
 
     renderInterface() {
-        const $root = document.getElementById('root');
-        $root.appendChild(createElement(navPanel));
+        this._root.appendChild(this._createNode(navPanel));
 
-        const getData = document.getElementById('js-get-data');
-        const update = document.getElementById('js-update-data');
-        const updateNode = document.getElementById('js-update-node');
-
-        getData.addEventListener('click', () => {
-            app.getData
+        document.getElementById('js-get-data').addEventListener('click', () => {
+            this.getData
         });
 
-        update.addEventListener('click', () => {
-            app.update(store)
+        document.getElementById('js-update-data').addEventListener('click', () => {
+            this.update(store)
         });
 
-        updateNode.addEventListener('click', () => {
-            app.updateNode(221, oneNode);
+        document.getElementById('js-update-node').addEventListener('click', () => {
+            this.updateNode(221, oneNode);
         });
     }
 
     render () {
-        function renderState(state) {
-            for(let i = 0; i < state.length; i++) {
-                const $el = document.createElement('div');
-                
-                const $root = document.getElementById('root');
-                $root.appendChild($el);
-            }
-        }
-
-        renderState(this._state)
+        this._root.appendChild(this._createNode(this._state));
     }
 }
 
 const app = new App(document.getElementById('root'), store) 
 app.renderInterface();
 app.render();
+
 
 
 
